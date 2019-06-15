19433c91428d8a9f7ff39112578cec65cf44b581
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/calculation/MainTest.java
+++ src/test/java/com/github/hcsp/calculation/MainTest.java
@@ -1,43 +1,21 @@
 package com.github.hcsp.calculation;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import java.io.*;
-import java.util.stream.Stream;
-import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
-
     @Test
     public void test() throws Exception {
         int number = new java.util.Random().nextInt(10000);
 
-        assertEquals(number / 16, Main.divide16(number));
-        assertEquals(number * 16, Main.multiply16(number));
-        assertEquals(-16 / 16, Main.divide16(-16));
-        assertEquals(-1 * 16, Main.multiply16(-1));
-        assertFalse(readFileContent().contains("*"));
-        assertFalse(readFileContent().replaceAll("/", "").contains("/"));
-    }
+        Assertions.assertEquals(number / 16, Main.divide16(number));
+        Assertions.assertEquals(number * 16, Main.multiply16(number));
+        Assertions.assertEquals(-16 / 16, Main.divide16(-16));
+        Assertions.assertEquals(-1 * 16, Main.multiply16(-1));
 
-    public String readFileContent() throws Exception {
-        Stream<String> entries =
-                Stream.of(System.getProperty("java.class.path").split(File.pathSeparator));
-        File targetClassDir =
-                entries.filter(
-                                entry ->
-                                        entry.endsWith("target/classes")
-                                                || entry.endsWith("target\\classes"))
-                        .findFirst()
-                        .map(File::new)
-                        .orElseThrow(IllegalStateException::new);
-        File mainDotJava =
-                new File(
-                        targetClassDir,
-                        "../../src/main/java/com/github/hcsp/calculation/Main.java");
-
-        return IOUtils.toString(new FileReader(mainDotJava));
+        String mainJava =
+                com.github.hcsp.test.helper.ProjectSourceFileReader.readAsString(Main.class);
+        Assertions.assertFalse(mainJava.contains("*"));
+        Assertions.assertFalse(mainJava.replaceAll("/", "").contains("/"));
     }
 }
