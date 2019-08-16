38537f363ca68c2cf4679e4f2ec841ce3e9644a1
yanghanjie <yang_hanjie@163.com>
2019-08-16 06:32:23 -0400

    Implement multiply and divide function with bit operators

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -3,10 +3,20 @@ package com.github.hcsp.calculation;
 public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
-    public static int multiply16(int number) {}
+    public static int multiply16(int number) {
+        return number << 4;
+    }
+
     // 返回number除以16的结果
     // 请不要使用除号
-    public static int divide16(int number) {}
+    public static int divide16(int number) {
+        if (number > 0)
+            return number >>> 4;
+        else if (number == 0)
+            return 0;
+        else
+            return (number >>> 4) | 0xf0000000;
+    }
 
     public static void main(String[] args) {
         System.out.println(multiply16(10));
