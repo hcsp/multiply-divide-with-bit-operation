f110707a48c14830d6918ea4086c853933ec985e
柯良勇 <keliangyong@gmail.com>
2019-08-30 11:09:01 +0800

    done

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -3,13 +3,25 @@ package com.github.hcsp.calculation;
 public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
-    public static int multiply16(int number) {}
+    public static int multiply16(int number) {
+        int result = Math.abs(number) << 4;
+        if((number ^ 16) < 0) {
+            result = ~result + 1;
+        }
+        return result;
+    }
     // 返回number除以16的结果
     // 请不要使用除号
-    public static int divide16(int number) {}
+    public static int divide16(int number) {
+        int result = Math.abs(number) >> 4;
+        if((number ^ 16) < 0) {
+            result = ~result + 1;
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        System.out.println(multiply16(10));
-        System.out.println(divide16(1600));
+        System.out.println(multiply16(-10));
+        System.out.println(divide16(-1600));
     }
 }
