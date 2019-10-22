da38c7621b08977323971f0c115d8a2559f48428
Lian <L1anyX@126.com>
2019-10-22 16:04:59 +0800

    使用位运算完成了乘除法

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,12 +1,20 @@
 package com.github.hcsp.calculation;
 
+import java.math.BigInteger;
+
 public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
-    public static int multiply16(int number) {}
+    public static int multiply16(int number) {
+        //左移一次相当于×2
+        return number << 4;
+    }
+
     // 返回number除以16的结果
     // 请不要使用除号
-    public static int divide16(int number) {}
+    public static int divide16(int number) {
+        return number >> 4;
+    }
 
     public static void main(String[] args) {
         System.out.println(multiply16(10));
