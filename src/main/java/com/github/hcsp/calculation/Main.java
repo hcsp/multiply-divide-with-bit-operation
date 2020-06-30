121d90248a18f5cd89d80a1e9cd006f601c5ac0c
2014 <suhanma@gmail.com>
2020-06-29 21:37:39 -0700

    Divide with bit (#186)
    Divide with bit (#186)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -3,10 +3,14 @@ package com.github.hcsp.calculation;
 public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
-    public static int multiply16(int number) {}
+    public static int multiply16(int number) {
+        return  number << 4;
+    }
     // 返回number除以16的结果
     // 请不要使用除号
-    public static int divide16(int number) {}
+    public static int divide16(int number) {
+        return number >> 4;
+    }
 
     public static void main(String[] args) {
         System.out.println(multiply16(10));
