12e037a325cd2ff8716ca5a10b4dbb4a8f4c80da
Bowen Wu <bwwu@iflytek.com>
2021-06-01 10:51:58 +0800

    feat: use >> & <<

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -3,10 +3,18 @@ package com.github.hcsp.calculation;
 public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
-    public static int multiply16(int number) {}
+    public static int multiply16(int number) {
+        return number << 4;
+    }
+
     // 返回number除以16的结果
     // 请不要使用除号
-    public static int divide16(int number) {}
+    public static int divide16(int number) {
+        if (number >= (int) Math.pow(2, 4)) {
+            return number >> 4;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
         System.out.println(multiply16(10));
