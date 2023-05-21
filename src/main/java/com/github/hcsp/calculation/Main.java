b80c7075fa06a3c8ef6916b2cf68f3f7513f1099
spearhead0428 <hastur1016@gmail.com>
2023-05-22 00:19:55 +0800

    12312311233

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -4,12 +4,12 @@ public class Main {
     // 返回number乘以16的结果
     // 请不要使用乘号
     public static int multiply16(int number) {
-        return number << 8;
+        return number << 4;
     }
     // 返回number除以16的结果
     // 请不要使用除号
     public static int divide16(int number) {
-        return number >> 8;
+        return number >> 4;
     }
 
     public static void main(String[] args) {
